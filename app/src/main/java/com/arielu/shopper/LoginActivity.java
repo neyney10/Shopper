9fb061fed59be0483768bffea91c92c4f760bf31
neyney10 <neyney10@live.com>
2019-11-27 11:56:49 +0200

    Adds Login activity with layout and adds strings to strings.xml and some styles in styles.xml for he login activity

---
+++ app/src/main/java/com/arielu/shopper/LoginActivity.java
@@ -0,0 +1,54 @@
+
+package com.arielu.shopper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class LoginActivity extends AppCompatActivity  {
+
+    //// Private variables - UI Views. ////
+    private EditText editTextEmail, editTextPassword;
+    private Button btnLogin, btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_srceen);
+
+        UILink();
+    }
+
+    /**
+     * Links UI views (elements) to object variables in the class.
+     */
+    private void UILink()
+    {
+        this.editTextEmail    = findViewById(R.id.email);
+        this.editTextPassword = findViewById(R.id.password);
+    }
+
+    /**
+     * Login the user using editTextEmail and editTextPassword to the service.
+     * @param view  - the button clicked
+     */
+    public void LogInClick(View view)
+    {
+        // TODO
+    }
+
+    /**
+     * Register the user using editTextEmail and editTextPassword to the service.
+     * @param view
+     */
+    public void RegisterClick(View view)
+    {
+        // TODO
+    }
+
+
+}
