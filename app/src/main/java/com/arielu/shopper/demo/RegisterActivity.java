e351cc71b4e7c90ae43f18dc364bef47f1925151
lichy24 <adi_lichy@yahoo.com>
2019-12-19 12:08:28 +0200

    update to RegisterActivity
    +edited RegisterActivity.java:
    now EditText will be check for validation:
    1)name must not be empty
    2)email must contain @
    3)password must have atleast length of 6
    4)confiremPassword must be the same as charsequence as password
    5)phoneNumber must be exactly 10 digits
    edited activity_register.xml,colors.xml,styles.xml:
    register UI and fix few reapeting lines in sytles.xml

--- app/src/main/java/com/arielu/shopper/demo/RegisterActivity.java
+++ app/src/main/java/com/arielu/shopper/demo/RegisterActivity.java
@@ -1,12 +1,16 @@
 package com.arielu.shopper.demo;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.arielu.shopper.demo.database.Firebase;
@@ -17,12 +21,17 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.regex.Pattern;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class RegisterActivity extends AppCompatActivity {
     private Spinner spinner;
     private EditText email,password,confirmPassword,phoneNumber,name;
+    private TextView emailText,passwordText,confirmPasswordText, phoneNumberText,nameText;
+    private boolean isValidEmail,isValidPassword,isValidConfirmPassword,isValidPhoneNumber,isValidName;
     private FirebaseAuth mAuth;
     private User userData;
     private FirebaseUser user;
@@ -33,7 +42,7 @@ public class RegisterActivity extends AppCompatActivity {
         init();
     }
     public void init(){
-        //spinner
+        //Spinner
         spinner = findViewById(R.id.user_type);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner_items,android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
@@ -44,8 +53,126 @@ public class RegisterActivity extends AppCompatActivity {
         confirmPassword = findViewById(R.id.confirm_password_field);
         phoneNumber = findViewById(R.id.phone_number_field);
         name = findViewById(R.id.name_field);
+        //TextView
+        emailText = findViewById(R.id.enter_email);
+        passwordText = findViewById(R.id.enter_password);
+        confirmPasswordText = findViewById(R.id.enter_confirm_password);
+        phoneNumberText = findViewById(R.id.enter_phone_number);
+        nameText = findViewById(R.id.enter_name);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+        //Add validation to EditText Input
+        fieldsValidation();
+    }
+    private void fieldsValidation(){
+
+        int pantone = ContextCompat.getColor(getApplicationContext(), R.color.pantone);
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                isValidName=!name.getText().toString().isEmpty();
+                if (isValidName){
+                    if (nameText.getCurrentTextColor()==pantone)
+                        nameText.setTextColor(Color.BLACK);
+                }else{
+                    nameText.setTextColor(pantone);
+                }
+            }
+        });
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+                isValidEmail = email.getText().toString().contains("@");
+                if(isValidEmail) {
+                    if (emailText.getCurrentTextColor()==pantone)
+                        emailText.setTextColor(Color.BLACK);
+                }else{
+                    emailText.setTextColor(pantone);
+                }
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                isValidPassword=Pattern.matches("^.{6,}$",password.getText());
+                if (isValidPassword){
+                    if (passwordText.getCurrentTextColor()==pantone)
+                        passwordText.setTextColor(Color.BLACK);
+                }else{
+                    passwordText.setTextColor(pantone);
+                }
+            }
+        });
+        confirmPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                isValidConfirmPassword=confirmPassword.getText().toString().equals(password.getText().toString());
+                if (isValidConfirmPassword){
+                    if (confirmPasswordText.getCurrentTextColor()==pantone)
+                        confirmPasswordText.setTextColor(Color.BLACK);
+                }else{
+                    confirmPasswordText.setTextColor(pantone);
+                }
+            }
+        });
+        phoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                isValidPhoneNumber = Pattern.matches("^\\d{10}$",phoneNumber.getText().toString());
+                if (isValidPhoneNumber){
+                    if (phoneNumberText.getCurrentTextColor()==pantone)
+                        phoneNumberText.setTextColor(Color.BLACK);
+                }else{
+                    phoneNumberText.setTextColor(pantone);
+                }
+            }
+        });
     }
     private void updateUserUI(FirebaseUser currentUser) {
         // if not connected then "currentUser" is equal to null
@@ -87,11 +214,14 @@ public class RegisterActivity extends AppCompatActivity {
                     }
                 });
     }
-
     public void Register(View view){
-                createNewAccountFirebase(this.email.getText().toString(),
-                        this.password.getText().toString(),this.name.getText().toString()
-                        ,this.phoneNumber.getText().toString(),this.spinner.getSelectedItemPosition());
-        Toast.makeText(getApplicationContext(), "you have been registered", Toast.LENGTH_SHORT).show();
+        if (isValidEmail&&isValidName&&isValidPassword&&isValidConfirmPassword&&isValidPhoneNumber) {
+            createNewAccountFirebase(this.email.getText().toString(),
+                    this.password.getText().toString(), this.name.getText().toString()
+                    , this.phoneNumber.getText().toString(), this.spinner.getSelectedItemPosition());
+            Toast.makeText(getApplicationContext(), "you have been registered.", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "can not register until all input fields are valid.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
