c30f94a2492dc210b4ca7f8b86c17b2ba4adf1e0
neyney10 <neyney10@live.com>
2020-01-09 19:35:19 +0200

    Adds basic functionality of adding and viewing list sharing permissions, as well see the shared list in the same place as your own lists for easy access- W.I.P

---
+++ app/src/main/java/com/arielu/shopper/demo/DialogAddPermission.java
@@ -0,0 +1,36 @@
+package com.arielu.shopper.demo;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.fragment.app.DialogFragment;
+
+public class DialogAddList extends DialogFragment {
+
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_add_list, null))
+                // Add action buttons
+                .setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                })
+                .setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
