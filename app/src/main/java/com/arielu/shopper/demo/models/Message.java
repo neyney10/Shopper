bfa87a088783658eda41d13ca307d33ca853c32e
neyney10 <neyney10@live.com>
2020-01-13 17:36:12 +0200

    [fix] fix for the previous commit, which didnt push to the firebase the correct companybranchID for user session store. [UI] now correctly show date of message in messageboardActivity in the format of dd-MM-yyyy instead of a timestamp in milliseconds

--- app/src/main/java/com/arielu/shopper/demo/models/Message.java
+++ app/src/main/java/com/arielu/shopper/demo/models/Message.java
@@ -1,5 +1,10 @@
 package com.arielu.shopper.demo.models;
 
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Message {
     private String title,content,date;
 
@@ -34,5 +39,14 @@ public class Message {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public String retrieveParsedDate()
+    {
+
+        Timestamp ts = new Timestamp(Long.parseLong(getDate()));
+        Date date = new Date(ts.getTime());
+        DateFormat f = new SimpleDateFormat("dd-MM-yyyy");
+        return f.format(date);
     }
 }
